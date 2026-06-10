2f6120534a8fb93fb223f23628f0a097a8c82173
Dan Chen <danchen@dhcp75.IfA.Hawaii.Edu>
2026-06-09 16:16:44 -1000

    Lots of changes

--- rebound_sims.py
+++ rebound_sims.py
@@ -223,7 +223,7 @@ def generate_params(planet_names, rng):
     return m_vals, r_vals, m_star, r_star, initial_P_ratios
 
 # From Izidoro 2014
-def get_tau(rock, rock_name, Sigma_1au=None, K_factor=None):
+def get_tau(rock, rock_name, Sigma_1au=None, h_1au=None):
     """Calculates damping timescales given parameters for one rock.
     Based on formulas from Izidoro 2014, Brasser 2007, and Adachi 1976.
     Planets and embryos are affected by migration while planetesimals
@@ -236,7 +236,7 @@ def get_tau(rock, rock_name, Sigma_1au=None, K_factor=None):
         rock_name (str): Name of the particle.
         Sigma_1au (float): Disk surface density at 1 AU, given in units
         of g/cm^2.
-        K_factor (float): Factor which equals tau_a/tau_e.
+        h_1au (float): Aspect ratio (scale height at 1 AU).
 
     Returns:
         tuple: Tuple containing tau_a, tau_e, tau_i for the given object,
@@ -254,14 +254,12 @@ def get_tau(rock, rock_name, Sigma_1au=None, K_factor=None):
         Sigma_1au *= AU**2 / Msun # Converted to Msun/AU^2 from g/cm^2
         Sigma_g = Sigma_1au * r**(-3/2)
         alpha = 1.5 # flaring index
-        h1 = ((2.7+1.1*alpha) / 0.780 * K_factor)**(-1/2)
-        h = h1 * r**1.25 # scale height
+        h = h_1au * r**1.25 # scale height
         
         t_a = (2/(2.7+1.1*alpha)) * (1/m_p) * (1/(Sigma_g*a_p**2)) * (h/r)**2 * ((1 + (e*r/(1.3*h))**5) / (1 - (e*r/(1.1*h))**4)) / Omega_k
         t_wave = (1/m_p) * (1/(Sigma_g*a_p**2)) * (h/r)**4 / Omega_k
         t_e = (t_wave/0.780) * (1 - 0.14*(e/(h/r))**2 + 0.06 * (e/(h/r))**3 + 0.18 * (e/(h/r)) * (inc/(h/r))**2)
         t_i = (t_wave/0.544) * (1 - 0.3*(inc/(h/r))**2 + 0.24 * (inc/(h/r))**3 + 0.14 * (e/(h/r))**2 * (inc/(h/r)))
-        
         return -t_a, -t_e, -t_i # Negative so damping
     
     elif 'embryo' in rock_name:
@@ -275,8 +273,7 @@ def get_tau(rock, rock_name, Sigma_1au=None, K_factor=None):
         Sigma_1au *= AU**2 / Msun # Converted to Msun/AU^2 from g/cm^2        
         Sigma_g = Sigma_1au * r**(-3/2)
         alpha = 1.5 # flaring index
-        h1 = ((2.7+1.1*alpha) / 0.780 * K_factor)**(-1/2)
-        h = h1 * r**1.25 # scale height
+        h = h_1au * r**1.25 # scale height
         
         t_a = (2/(2.7+1.1*alpha)) * (1/m_p) * (1/(Sigma_g*a_p**2)) * (h/r)**2 * ((1 + (e*r/(1.3*h))**5) / (1 - (e*r/(1.1*h))**4)) / Omega_k
         t_wave = (1/m_p) * (1/(Sigma_g*a_p**2)) * (h/r)**4 / Omega_k
@@ -293,20 +290,19 @@ def get_tau(rock, rock_name, Sigma_1au=None, K_factor=None):
         Omega_k = 2*np.pi/(rock.P*yr) # 1/s
         
         Sigma_g = Sigma_1au * (r/AU)**(-3/2) # g/cm^2
-        alpha_flaring = 1.5 # flaring index
-        h1 = ((2.7+1.1*alpha_flaring) / 0.780 * K_factor)**(-1/2)
-        h = h1 * (r/AU)**1.25 # scale height in AU
+            
+        h = h_1au * (r/AU)**1.25 # scale height in AU
         v_K = r*Omega_k # cm/s
-
+        
         m_p = 0.0033 * u.Mearth.to(u.g) # g
         rho_p = m_p / (4/3 * np.pi * R_p**3) # Ptsml density (mass / vol) in g/cm^3
         C_d = 0.44 # for km-sized ptsmls and small Mach number (see Brasser 2007) 
-        rho_g = Sigma_1au/(np.sqrt(np.pi)*(h1*AU)) * (r/AU)**(-11/4) # g/cm^3
+        rho_g = Sigma_1au/(np.sqrt(np.pi)*(h_1au*AU)) * (r/AU)**(-11/4) # g/cm^3
         eta = (11/16)*(h*AU/r)**2
-
+        
         K = 2.157
         E = 1.211
-        alpha = 3 # constant in Adachi model; NOT flaring index
+        alpha = 3 # constant in Adachi model (NOT flaring index)
         
         t_0 = (8*rho_p*R_p) / (3*C_d*rho_g*v_K) / yr # yr
         t_a = t_0 / (2 * ((2*(2*E+K)/(3*np.pi)*e + 2/np.pi*inc + eta) * eta + 
@@ -314,7 +310,6 @@ def get_tau(rock, rock_name, Sigma_1au=None, K_factor=None):
                             (inc**3)/(2*np.pi))) # Adachi 1976 Eq. 4.11
         t_e = t_0 / ((2*E)/(np.pi)*e + 2/np.pi*inc + eta) # Adachi 1976 Eq. 4.12
         t_i = t_0 / (1/2 * (2*E/np.pi*e + 8/(3*np.pi)*inc + eta)) # Adachi 1976 Eq. 4.13
-        
         return -t_a, -t_e, -t_i
     
 # === RUNNING THE SIM ===
@@ -340,7 +335,7 @@ def integrate_sim(sim, rocks, rock_names, parameters, years, start_time=0):
             * stage_data (pd.Dataframe): Simulation data.
             * completed_sim (bool): Whether the integration was fully compelted.
     '''
-    m_vals, m_star, r_vals, r_star, Sigma_1au, K_factor = parameters["m_vals"], parameters["m_star"], parameters["r_vals"], parameters["r_star"], parameters["Sigma_1au"], parameters["K_factor"]
+    m_vals, m_star, r_vals, r_star, Sigma_1au, h_1au = parameters["m_vals"], parameters["m_star"], parameters["r_vals"], parameters["r_star"], parameters["Sigma_1au"], parameters["h_1au"]
     num_pl, num_em, num_ptsml = parameters["num_pl"], parameters["num_em"], parameters["num_ptsml"]
     num_rocks = len(rock_names)
     
@@ -370,12 +365,13 @@ def integrate_sim(sim, rocks, rock_names, parameters, years, start_time=0):
 
     for i, t in enumerate(stage_times): 
         
+        alive_rocks = []
         for j, name in enumerate(rock_names):
 
             try: # Prevent error for removed particles
                 rock = sim.particles[name]
                 
-                tau_a, tau_e, tau_i = get_tau(rock, name, Sigma_1au, K_factor)
+                tau_a, tau_e, tau_i = get_tau(rock, name, Sigma_1au, h_1au)
                 rock.params["tau_a"] = tau_a
                 rock.params["tau_e"] = tau_e
                 rock.params["tau_inc"] = tau_i
@@ -396,10 +392,12 @@ def integrate_sim(sim, rocks, rock_names, parameters, years, start_time=0):
                     hist["P_ratio"][i, j] = rocks[j+1].P / rock.P # index might be wrong if planet is gone
                 
                 # Remove particle if planet goes into star
-                if rock.d < 0.1 or rock.d > 100:
+                if rock.d < 0.05 or rock.d > 100:
                     sim.remove(hash=name)
-                    print(f"{name} removed")
-            
+                    # print(f"{name} removed")
+                    
+                alive_rocks.append(rock)
+                    
             except rebound.ParticleNotFound as error:
                 pass
 
@@ -416,7 +414,7 @@ def integrate_sim(sim, rocks, rock_names, parameters, years, start_time=0):
             #         break
                 
         print(f"Step {i} of {len(stage_times)}", end="\r", flush=True)
-        sim.dt = np.min((rocks[0].P / 20, 1/20)) # 1/20 or planet b or simply 1/20 yrs (P at 1AU)
+        sim.dt = np.min([rock.P / 20 for rock in alive_rocks]) # 1/20 of innermost particle 
         sim.integrate(t)    
         
         # Stop sim if planet b is gone       
@@ -456,11 +454,11 @@ def simulate_system(sim_id, file_path, rock_names, parameters, years=None, integ
         rock_names (list[str]): Names of the rocks (non-stellar particles).    
         parameters (dict): Dictionary containing planet and star parameters.
         years (float, optional, defaults to None): Number of years to integrate the simulation.
-            If none is given, will use max(tau_a of the last planet) and clip within (30kyr, 10Myr).
+            If none is given, will use 0.8*tau_a of the last planet and clip within (30kyr, 10Myr).
         integrator (str, optional, defaults to whfast): Name of the REBOUND integrator to use.
     """    
     m_vals, m_star, r_vals, r_star, a_vals = parameters["m_vals"], parameters["m_star"], parameters["r_vals"], parameters["r_star"], parameters["a_vals"]
-    ide_position, ide_width, Sigma_1au, K_factor = parameters["ide_position"], parameters["ide_width"], parameters["Sigma_1au"], parameters["K_factor"]
+    ide_position, ide_width, Sigma_1au, h_1au = parameters["ide_position"], parameters["ide_width"], parameters["Sigma_1au"], parameters["h_1au"]
     num_pl, num_em, num_ptsml = parameters["num_pl"], parameters["num_em"], parameters["num_ptsml"]
     
     # Create the simulation
@@ -560,11 +558,13 @@ def simulate_system(sim_id, file_path, rock_names, parameters, years=None, integ
     mof.params["ide_position"] = ide_position
     mof.params["ide_width"] = ide_width
 
-    if years is None: # Set to tau_a of the last planet
-        years = -get_tau(rocks[num_pl-1], rock_names[num_pl-1], Sigma_1au, K_factor)[0]
+    if years is None: # Set to 0.8*tau_a of the last planet
+        years = -0.8*get_tau(rocks[num_pl-1], rock_names[num_pl-1], Sigma_1au, h_1au)[0]
 
     # Clip
-    years = np.clip(years, 30e3, 10e6)
+    if years < 1e3 or years > 1e6:
+        years = np.clip(years, 1e3, 1e6)
+        print(f"Years clipped to {years:.3e}")
         
     print(f"Sim {sim_id:<2d} | {years:.3g} years", flush=True)
     data, complete_sim = integrate_sim(sim, rocks, rock_names, parameters, years, start_time=0)
@@ -580,13 +580,13 @@ def simulate_system(sim_id, file_path, rock_names, parameters, years=None, integ
  
 # === SIM SETUP ===       
 
-num_pl = 4
-num_em = 40
-num_ptsml = 500
+num_pl = 3
+num_em = 2
+num_ptsml = 2
 
-rock_names = ['planet b', 'planet c', 'planet d', 'planet e'] + [f"embryo {i}" for i in range(num_em)] + [f"ptsml {i}" for i in range(num_ptsml)]
+rock_names = ['planet b', 'planet c', 'planet d'] + [f"embryo {i}" for i in range(num_em)] + [f"ptsml {i}" for i in range(num_ptsml)]
 dataset_id = 2
-n_sims = 100
+n_sims = 1
 
 def run_sim(sim_id):
     # Different rng for each sim
@@ -600,21 +600,27 @@ def run_sim(sim_id):
     # ALTERNATIVELY: Specify values below: 
     
     # === PARAMETERS ===
-    # Planets take Kepler-223 values
-    m_vals = np.array([6.6, 4.5, 7.1, 4.3] + [0.01]*num_em + [0.0033]*num_ptsml) * m_earth
-    r_vals = np.array([3.0, 3.4, 5.2, 4.6] + [0.2]*(num_em) + [(100*1e5/AU)/r_earth]*num_ptsml) * r_earth
+    # Planets take Kepler-223 b/c/d values
+    m_vals = np.array([6.6, 4.5, 7.1] + [0.03]*num_em + [0.0033]*num_ptsml) * m_earth
+                    # Younger planet is puffier
+    r_vals = np.array(2*[3.0, 3.4, 5.2] + [0.3]*(num_em) + [(100*1e5/AU)/r_earth]*num_ptsml) * r_earth
     m_star = 1.04 # Msun
-    r_star = 1.52 * r_sun # AU
-    a_vals = np.concatenate(([4, 5, 6.85, 8.57], np.arange(1, 3, 2/num_em), np.arange(1.05, 3.05, 2/num_ptsml))) # Initial a_vals
-    ide_position = 0.1 # a bit above where K-223b sits
+    r_star = 1.52 * r_sun
+    a_vals = np.concatenate(([2, 3.2, 4.85], np.linspace(0.55, 1.45, num_em), np.linspace(0.5, 1.5, num_ptsml))) # Initial a_vals
+    ide_position = 0.2 # a bit above where K-223b sits
     ide_width = 0.02
-
-    Sigma_1au = np.tile(np.logspace(3, 4, num=10), 10)[sim_id] # Each row is the same
-    K_factor = np.repeat(np.logspace(1, 3, num=10), 10)[sim_id] # Each column is the same
     
-    # Manually set
-    # Sigma_1au = 1000
-    # K_factor = 1000
+    Sigma_1au = 1700 * np.tile(np.logspace(-1, 1, num=5), 5)[sim_id] # Each row is the same
+    
+    # K_factor = np.repeat(np.logspace(1.5, 2.5, num=5), 5)[sim_id] # Each column is the same
+    # K_factor = 100
+    # alpha = 1.5
+    # h_1au = ((2.7+1.1*alpha) / 0.780 * K_factor)**(-1/2)
+    
+    h_1au = np.repeat(np.logspace(-2, -1, num=5), 5)[sim_id] # Each column is the same
+    
+    Sigma_1au = 1000
+    h_1au = 0.03
         
     parameters = {"m_vals": m_vals,
                   "m_star": m_star,
@@ -627,43 +633,48 @@ def run_sim(sim_id):
                   "ide_position": ide_position,
                   "ide_width": ide_width,
                   "Sigma_1au": Sigma_1au,
-                  "K_factor": K_factor
+                  "h_1au": h_1au
                 }
     
     # Sim integration!
     outcome = simulate_system(sim_id, file_path, rock_names, parameters, integrator="trace")
     return (sim_id, m_vals, r_vals, m_star, r_star)
     
-# === MULTIPROCESSING ===    
-
 if __name__ == "__main__":
     dataset_dir = Path.cwd().parent / "sim_results" / f"dataset{dataset_id}"
     
     # Create the folder
     dataset_dir.mkdir(parents=True, exist_ok=True) # change to False to be safe
-    print(f"Created directory: {dataset_dir}")
+    print(f"Directory: {dataset_dir}")
 
     print(f"Dataset: {dataset_id}")
     tstart = time()
 
-    # Start a local Dask cluster
-    n_cpus = int(os.environ.get("SLURM_CPUS_PER_TASK", 1))
-    print(f"CPUs: {n_cpus}")
-    cluster = LocalCluster()    
-    client = Client(cluster)
+    # === MULTIPROCESSING ===    
+    multiprocess = False
     
-    print(f"Running sims on {len(client.scheduler_info()['workers'])} workers")
-    print(f"Dask dashboard: {client.dashboard_link}")
-
-    try:
-        # Submit all simulations as Dask futures
-        futures = [client.submit(run_sim, sim_id) for sim_id in range(n_sims)]
+    if multiprocess:
+        # Start a local Dask cluster
+        n_cpus = int(os.environ.get("SLURM_CPUS_PER_TASK", 1))
+        print(f"CPUs: {n_cpus}")
+        cluster = LocalCluster()    
+        client = Client(cluster)
         
-        # Gather results (blocks until all futures are complete)
-        outcomes = client.gather(futures)
-    finally:
-        client.close()
-        cluster.close()
+        print(f"Running sims on {len(client.scheduler_info()['workers'])} workers")
+        print(f"Dask dashboard: {client.dashboard_link}")
+
+        try:
+            # Submit all simulations as Dask futures
+            futures = [client.submit(run_sim, sim_id) for sim_id in range(n_sims)]
+            
+            # Gather results (blocks until all futures are complete)
+            outcomes = client.gather(futures)
+        finally:
+            client.close()
+            cluster.close()
+    else: # Don't use Dask, do one sim
+        sim_id = 0
+        outcomes = [run_sim(sim_id)]
     
     # Save the outcomes
     outcome_file = f"../sim_results/dataset{dataset_id}/outcomes.pkl"
