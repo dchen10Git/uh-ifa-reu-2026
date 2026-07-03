8a3e61a048e5110a4b8313597a323a5f261710fc
Dan Chen <danchen@Dans-MacBook-Pro.local>
2026-07-02 20:20:06 -1000

    Added MMR labels to cdf video

--- plot_cdf_video.py
+++ plot_cdf_video.py
@@ -81,6 +81,24 @@ def animate_cumulative_mass_distribution(sim_id, dataset_id=7, n_frames=100,
 
         return locations
 
+    def get_interior_resonance_locations(planet_locations):
+        resonance_locations = {}
+        resonances = {
+            "2:1": 2 / 1,
+            "3:2": 3 / 2,
+            "4:3": 4 / 3,
+            "5:4": 5 / 4,
+            "6:5": 6 / 5,
+        }
+
+        for planet_name, planet_a in planet_locations.items():
+            resonance_locations[planet_name] = {
+                res_name: planet_a * period_ratio**(-2 / 3)
+                for res_name, period_ratio in resonances.items()
+            }
+
+        return resonance_locations
+
     base_dir = Path.cwd()
     file_path = base_dir.parent / f"sim_results/dataset{dataset_id}" / f"sim{sim_id}.h5"
     saved_sim = reb_sims.load_simulation_run(file_path)
@@ -102,6 +120,11 @@ def animate_cumulative_mass_distribution(sim_id, dataset_id=7, n_frames=100,
         for idx in range(n_frames)
     ]
 
+    resonance_locations_by_frame = [
+        get_interior_resonance_locations(locations)
+        for locations in planet_locations_by_frame
+    ]
+
     # Fixed plot limits to avoid jitter
     all_a = np.concatenate([
         a for a, M in distributions
@@ -119,7 +142,14 @@ def animate_cumulative_mass_distribution(sim_id, dataset_id=7, n_frames=100,
         for aval in locations.values()
     ])
 
-    xvals = np.concatenate([all_a, all_planet_a])
+    all_resonance_a = np.asarray([
+        aval
+        for planet_locations in resonance_locations_by_frame
+        for resonance_locations in planet_locations.values()
+        for aval in resonance_locations.values()
+    ])
+
+    xvals = np.concatenate([all_a, all_planet_a, all_resonance_a])
     xmin = max(0.1, np.nanmin(xvals) * 0.8)
     xmax = np.nanmax(xvals) * 1.2
     ymax = np.nanmax(all_M) * 1.05
@@ -130,6 +160,8 @@ def animate_cumulative_mass_distribution(sim_id, dataset_id=7, n_frames=100,
 
     planet_lines = {}
     planet_texts = {}
+    resonance_lines = {}
+    resonance_texts = {}
 
     planet_colors = {
         "planet b": "C0",
@@ -157,6 +189,30 @@ def animate_cumulative_mass_distribution(sim_id, dataset_id=7, n_frames=100,
             fontweight="bold"
         )
 
+        resonance_lines[name] = {}
+        resonance_texts[name] = {}
+
+        for res_name in ["2:1", "3:2", "4:3", "5:4", "6:5"]:
+            resonance_lines[name][res_name] = ax.axvline(
+                np.nan,
+                color=planet_colors[name],
+                ls=":",
+                lw=0.9,
+                alpha=0.45
+            )
+
+            resonance_texts[name][res_name] = ax.text(
+                1,
+                ymax * 0.88,
+                res_name,
+                color=planet_colors[name],
+                ha="center",
+                va="top",
+                fontsize=6,
+                alpha=0.75,
+                rotation=90
+            )
+
     time_text = ax.text(
         0.03,
         0.95,
@@ -180,6 +236,7 @@ def animate_cumulative_mass_distribution(sim_id, dataset_id=7, n_frames=100,
         line.set_data(a, M)
 
         locations = planet_locations_by_frame[frame]
+        resonance_locations = resonance_locations_by_frame[frame]
 
         for name in ["planet b", "planet c", "planet d"]:
             if name in locations:
@@ -187,9 +244,18 @@ def animate_cumulative_mass_distribution(sim_id, dataset_id=7, n_frames=100,
                 planet_lines[name].set_xdata([aval, aval])
                 planet_texts[name].set_position((aval, ymax * 0.96))
                 planet_texts[name].set_visible(True)
+
+                for res_name, res_aval in resonance_locations[name].items():
+                    resonance_lines[name][res_name].set_xdata([res_aval, res_aval])
+                    resonance_texts[name][res_name].set_position((res_aval, ymax * 0.88))
+                    resonance_texts[name][res_name].set_visible(True)
             else:
                 planet_lines[name].set_xdata([np.nan, np.nan])
                 planet_texts[name].set_visible(False)
+
+                for res_name in ["2:1", "3:2", "4:3", "5:4", "6:5"]:
+                    resonance_lines[name][res_name].set_xdata([np.nan, np.nan])
+                    resonance_texts[name][res_name].set_visible(False)
 
         try:
             t = sim_data[first_name]["time"].iloc[frame]
@@ -197,7 +263,25 @@ def animate_cumulative_mass_distribution(sim_id, dataset_id=7, n_frames=100,
         except Exception:
             time_text.set_text(f"sim {sim_id}, snapshot {frame}")
 
-        return [line, time_text, *planet_lines.values(), *planet_texts.values()]
+        resonance_line_artists = [
+            res_line
+            for planet_res_lines in resonance_lines.values()
+            for res_line in planet_res_lines.values()
+        ]
+        resonance_text_artists = [
+            res_text
+            for planet_res_texts in resonance_texts.values()
+            for res_text in planet_res_texts.values()
+        ]
+
+        return [
+            line,
+            time_text,
+            *planet_lines.values(),
+            *planet_texts.values(),
+            *resonance_line_artists,
+            *resonance_text_artists,
+        ]
 
     anim = FuncAnimation(
         fig,
@@ -226,9 +310,9 @@ assert len(sys.argv) == 3
 anim = animate_cumulative_mass_distribution(
     sim_id=sys.argv[2],
     dataset_id=sys.argv[1],
-    n_frames=100,
-    fps=15,
-    save_path=f"sim{sys.argv[2]}_cumulative_mass.mp4"
+    n_frames=1000,
+    fps=60,
+    save_path=f"Figures/mass_cdf_ds{sys.argv[1]}_sim{sys.argv[2]}.mp4"
 )
 
 # Usage: python3 plot_cdf_video.py <dataset_id> <sim_id>
